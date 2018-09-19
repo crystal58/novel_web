77aa976c877980d84dfcf626197aa25880695a26
zhangyapin <zhangyapin@yongche.com>
2018-09-19 14:00:26 +0800

    引入样式

--- public/js/public.js
+++ public/js/public.js
@@ -52,5 +52,43 @@ $(document).ready(function(){
         });
         $('#wos-pagination-prev-more').hide();
     });
+
+  var countnum=5 //一共多少个图 例如6个请输入5
+  $("#leftbtn").click(function(){
+    var temp_href=$("#topface li:eq(0) a").attr("href");
+    var temp_img=$("#topface li:eq(0) img").attr("src");
+    var temp_h3=$("#topface li:eq(0) h3").html();
+    var temp_p=$("#topface li:eq(0) p").html();
+
+    for (i=0; i<countnum; i++){
+      var n=i+1;
+      $("#topface li:eq("+i+") a").attr('href',$("#topface li:eq("+n+") a").attr("href"));
+      $("#topface li:eq("+i+") img").attr('src',$("#topface li:eq("+n+") img").attr("src"));
+      $("#topface li:eq("+i+") h3").html($("#topface li:eq("+n+") h3").html());
+      $("#topface li:eq("+i+") p").html($("#topface li:eq("+n+") p").html());
+    };
+    $("#topface li:eq("+countnum+") a").attr('href',temp_href);
+    $("#topface li:eq("+countnum+") img").attr('src',temp_img);
+    $("#topface li:eq("+countnum+") h3").html(temp_h3);
+    $("#topface li:eq("+countnum+") p").html(temp_p);
+  });
+  $("#rightbtn").click(function(){
+    var temp_href=$("#topface li:eq("+countnum+") a").attr("href");
+    var temp_img=$("#topface li:eq("+countnum+") img").attr("src");
+    var temp_h3=$("#topface li:eq("+countnum+") h3").html();
+    var temp_p=$("#topface li:eq("+countnum+") p").html();
+
+    for (i=countnum; i>0; i--){
+      var n=i-1;
+      $("#topface li:eq("+i+") a").attr('href',$("#topface li:eq("+n+") a").attr("href"));
+      $("#topface li:eq("+i+") img").attr('src',$("#topface li:eq("+n+") img").attr("src"));
+      $("#topface li:eq("+i+") h3").html($("#topface li:eq("+n+") h3").html());
+      $("#topface li:eq("+i+") p").html($("#topface li:eq("+n+") p").html());
+    };
+    $("#topface li:eq(0) a").attr('href',temp_href);
+    $("#topface li:eq(0) img").attr('src',temp_img);
+    $("#topface li:eq(0) h3").html(temp_h3);
+    $("#topface li:eq(0) p").html(temp_p);
+  });
 });
 
