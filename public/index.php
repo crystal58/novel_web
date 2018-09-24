74621327b0bc61f0dc134d02bbb5e36722bcf2cf
zhangyapin <zhangyapin@yongche.com>
2018-09-24 14:37:58 +0800

    seo

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 define('APPLICATION_PATH', dirname(__FILE__)."/../");
 $config = require APPLICATION_PATH."conf/dbConfig.php";
-
+error_reporting(0);
 Yaf_Registry::set("dbconfig",$config);
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 try{
