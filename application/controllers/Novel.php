fc70b74aa7443a5b5f2414cfcbd44990a5d719de
zhangyapin <zhangyapin@yongche.com>
2018-10-12 11:36:32 +0800

    bug

--- application/controllers/Novel.php
+++ application/controllers/Novel.php
@@ -153,7 +153,7 @@ class NovelController extends AbstractController{
                 "novel_id" => $novelId,
                 "status" => 1
             );
-            $chaptersList = $novelChapters->chaptersList($params,false);
+            $chaptersList = $novelChapters->chaptersList($params);
             $this->_view->list = $chaptersList['list'];
 
             $novelModel = new NovelModel();
